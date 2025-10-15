5b7f8368e4a237bce82e4689b6df473f7f98e657
ahsan-javaid <ahsan786cs@gmail.com>
2025-10-15 06:55:57 +0500

    fix: formatting fixes

--- src/lib.rs
+++ src/lib.rs
@@ -32,19 +32,17 @@ impl BigInt {
         while x > 0 {
             digits.push((x % BASE as u64) as u32);
             x /= BASE as u64;
-        } 
-        Self {
-            neg: v < 0, digits
-        }.normalize()
+        }
+        Self { neg: v < 0, digits }.normalize()
     }
 
     fn normalize(mut self) -> Self {
-        while self.digits.last().map_or(false, |&d| d == 0) { 
+        while self.digits.last().map_or(false, |&d| d == 0) {
             self.digits.pop();
         }
 
-        if self.digits.is_empty(){
-            self.neg = false; 
+        if self.digits.is_empty() {
+            self.neg = false;
         }
 
         self
@@ -85,7 +83,6 @@ impl BigInt {
 
         s
     }
-
 }
 
 pub fn add(left: u64, right: u64) -> u64 {
@@ -98,17 +95,17 @@ mod tests {
 
     #[test]
     fn it_works() {
-        println!("vec------: {:?}", vec![10,1]);
+        println!("vec------: {:?}", vec![10, 1]);
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
 
     #[test]
     fn from_i64() {
-       let v = BigInt::from_i64(1002323809800980);
-       for (i, &n) in [809800980, 1002323].iter().enumerate() {
-        assert_eq!(n, v.digits[i]);
-       }
+        let v = BigInt::from_i64(1002323809800980);
+        for (i, &n) in [809800980, 1002323].iter().enumerate() {
+            assert_eq!(n, v.digits[i]);
+        }
     }
 
     #[test]
